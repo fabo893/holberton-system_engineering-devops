788312a50417e64a19037cef0ec16f9e051f5a4a
fabo893 <jose.rosa893@gmail.com>
2021-04-05 03:36:11 +0000

    Client configuration file (w/Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,13 @@
 # Puppet SSH Config
+include stdlib
+file_line { 'Disable password authentication':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^*PasswordAuthentication*',
+}
+
+file_line { 'add identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/holberton',
+  match => '^*IdentityFile ~/xssh/holberton*',
+}
