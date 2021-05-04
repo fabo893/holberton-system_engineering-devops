a0f38cd5bc923130710177aa7ab36da57b0ec597
fabo893 <jose.rosa893@gmail.com>
2021-05-04 02:39:03 +0000

    testing

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,17 +5,23 @@
 
 
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    u = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+    u = requests.get('https://jsonplaceholder.typicode.com/users')
     d1 = u.json()
     td = requests.get('https://jsonplaceholder.typicode.com/todos')
     d2 = td.json()
 
-    EMPLOYEE_NAME = d1['name']
-    empid = d1['id']
+    arg = int(argv[1]) - 1
+
+    ul = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                      .format(arg + 1))
+    d3 = ul.json()
+
+    EMPLOYEE_NAME = d1[arg]['name']
+    empid = d1[arg]['id']
 
     total_res = 0
     for i in d2:
@@ -35,8 +41,11 @@ if __name__ == "__main__":
     n = NUMBER_OF_DONE_TASKS
     t = TOTAL_NUMBER_OF_TASKS
 
+    lis = []
+    for l in d3:
+        for k, v in l.items():
+            if k == 'completed' and v is True:
+                lis.append(l.get('title'))
+
     print("Employee {} is done with tasks({}/{}):".format(e, n, t))
-    for i in d2:
-        if i['userId'] == empid and i['completed'] is True:
-            title = i['title']
-            print("\t {}".format(title))
+    print("\n".join("\t {}".format(title) for title in lis))
