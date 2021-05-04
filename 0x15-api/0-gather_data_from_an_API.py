a8f7aea3b2d9f5f9722c26833fb7ac73eed624b2
fabo893 <jose.rosa893@gmail.com>
2021-05-04 01:47:35 +0000

    data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,37 +8,36 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    u = requests.get('https://jsonplaceholder.typicode.com/users')
-    d1 = u.json()
-    td = requests.get('https://jsonplaceholder.typicode.com/todos')
-    d2 = td.json()
+u = requests.get('https://jsonplaceholder.typicode.com/users')
+d1 = u.json()
+td = requests.get('https://jsonplaceholder.typicode.com/todos')
+d2 = td.json()
 
-    arg = int(sys.argv[1]) - 1
+arg = int(sys.argv[1]) - 1
 
-    EMPLOYEE_NAME = d1[arg]['name']
-    empid = d1[arg]['id']
+EMPLOYEE_NAME = d1[arg]['name']
+empid = d1[arg]['id']
 
-    total_res = 0
-    for i in d2:
-        if i['userId'] == empid:
-            total_res = total_res + 1
+total_res = 0
+for i in d2:
+    if i['userId'] == empid:
+        total_res = total_res + 1
 
-    TOTAL_NUMBER_OF_TASKS = total_res
+TOTAL_NUMBER_OF_TASKS = total_res
 
-    done = 0
-    for i in d2:
-        if i['userId'] == empid and i['completed'] is True:
-            done = done + 1
+done = 0
+for i in d2:
+    if i['userId'] == empid and i['completed'] is True:
+        done = done + 1
 
-    NUMBER_OF_DONE_TASKS = done
+NUMBER_OF_DONE_TASKS = done
 
-    e = EMPLOYEE_NAME
-    n = NUMBER_OF_DONE_TASKS
-    t = TOTAL_NUMBER_OF_TASKS
+e = EMPLOYEE_NAME
+n = NUMBER_OF_DONE_TASKS
+t = TOTAL_NUMBER_OF_TASKS
 
-    print("Employee {} is done with tasks({}/{}):".format(e, n, t))
-    for i in d2:
-        if i['userId'] == empid and i['completed'] is True:
-                title = i['title']
-                print("\t {}".format(title))
+print("Employee {} is done with tasks({}/{}):".format(e, n, t))
+for i in d2:
+    if i['userId'] == empid and i['completed'] is True:
+        title = i['title']
+        print("\t {}".format(title))
